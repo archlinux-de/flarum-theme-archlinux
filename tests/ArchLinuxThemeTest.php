62891c47b4ce91c64efab9d7bd7daf4fdfa75d22
Pierre Schmitz <pierre@archlinux.de>
2021-05-01 17:30:52 +0200

    Add basic test

---
+++ tests/ArchLinuxThemeTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ArchLinux\Theme\Tests;
+
+use ArchLinux\Theme\ArchLinuxTheme;
+use Flarum\Foundation\Config;
+use Flarum\Frontend\Document;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use PHPUnit\Framework\TestCase;
+
+class ArchLinuxThemeTest extends TestCase
+{
+    public function testRender(): void
+    {
+        $config = $this->createMock(Config::class);
+        $config
+            ->expects($this->atLeastOnce())
+            ->method('offsetGet')
+            ->with('arch')
+            ->willReturn([]);
+
+        $renderer = $this->createMock(Renderable::class);
+        $renderer
+            ->expects($this->atLeastOnce())
+            ->method('render')
+            ->willReturn('foo');
+
+        $viewFactory = $this->createMock(ViewFactory::class);
+        $viewFactory
+            ->expects($this->atLeastOnce())
+            ->method('make')
+            ->willReturn($renderer);
+
+        $forumApiDocument = [];
+        $document = $this->createMock(Document::class);
+        $document
+            ->expects($this->atLeastOnce())
+            ->method('getForumApiDocument')
+            ->willReturnCallback(
+                function () use (&$forumApiDocument) {
+                    return $forumApiDocument;
+                }
+            );
+        $document
+            ->expects($this->atLeastOnce())
+            ->method('setForumApiDocument')
+            ->willReturnCallback(
+                function (array $input) use (&$forumApiDocument) {
+                    $forumApiDocument = $input;
+                }
+            );
+
+        $archLinuxTheme = new ArchLinuxTheme($config, $viewFactory);
+        $archLinuxTheme($document);
+
+        $this->assertEquals('foo', $document->getForumApiDocument()['data']['attributes']['headerHtml']);
+        $this->assertEquals('foo', $document->getForumApiDocument()['data']['attributes']['footerHtml']);
+        $this->assertStringContainsString('rel="shortcut icon"', $document->head[0]);
+    }
+}
